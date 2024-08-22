99347bbc38c55848006e9f9760aae3cf6db35acd
Gowri Mohan <gowrimohan@Gowris-MacBook-Pro.local>
2024-08-22 12:09:38 +0530

    fifth commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
-/*
 using Microsoft.EntityFrameworkCore;
-using Myapp.data;
+using Myapp.data; // Ensure the correct namespace for UserContext
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.OpenApi.Models; // For Swagger
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,22 +11,42 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<UserContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Set up authentication using cookies
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-            .AddCookie(options =>
-            {
-                options.LoginPath = "/Account/Login";
-                options.AccessDeniedPath = "/Account/AccessDenied";
- 
-            });
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.AccessDeniedPath = "/Account/AccessDenied";
+    });
 
+// Add authorization policies
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+});
+
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyApp API", Version = "v1" });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyApp API V1");
+        c.RoutePrefix = "swagger"; // Swagger UI will be at /swagger
+    });
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -42,56 +62,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Account}/{action=Login}/{id?}");
 
-app.Run();
-*/
-
-using Microsoft.EntityFrameworkCore;
-using Myapp.data;
-using Microsoft.AspNetCore.Authentication.Cookies;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddDbContext<UserContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Set up authentication using cookies
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
-    {
-        options.LoginPath = "/Account/Login";
-        options.AccessDeniedPath = "/Account/AccessDenied";  // Redirect to Access Denied page if access is forbidden
-    });
-
-// Add authorization policies
-builder.Services.AddAuthorization(options =>
-{
-    // Define a policy that requires the "Admin" role
-    options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
-});
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication(); // Ensure that authentication is used
-app.UseAuthorization();  // Ensure that authorization is used
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
 
 app.Run();
 
