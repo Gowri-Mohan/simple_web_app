34cca6315e682409172c746e7d9153dd3c80dfc8
raghavendrajnath <ragha.j.nath@gmail.com>
2024-09-04 08:53:28 +0530

    added database in docker compose.

--- MyAppAPI/Program.cs
+++ MyAppAPI/Program.cs
@@ -57,6 +57,13 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
+using (var serviceScope = app.Services.GetService<IServiceScopeFactory>().CreateScope())
+    {
+        var context = serviceScope.ServiceProvider.GetRequiredService<UserContext>();
+        context.Database.Migrate();
+    }
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
